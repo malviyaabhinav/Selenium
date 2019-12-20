5aee5106f4fcaf5945ad66a931f3ba97dc55d2b4
Abhinav Kumar <abhinavmalviya1992@gmail.com>
2019-12-20 02:07:42 +0100

    Select dropdown by Index

--- src/main/java/seleniumBasics/Dropdown.java
+++ src/main/java/seleniumBasics/Dropdown.java
@@ -19,15 +19,24 @@ public class Dropdown {
 		WebElement day=driver.findElement(By.xpath("//select[@id='day']"));
 		WebElement month=driver.findElement(By.xpath("//select[@id='month']"));
 		WebElement year=driver.findElement(By.xpath("//select[@id='year']"));
-		selectByVisibleText(day, "25");
-		selectByVisibleText(month, "Nov");
-		selectByVisibleText(year, "1992");
+		/*
+		 * selectByVisibleText(day, "25"); selectByVisibleText(month, "Nov");
+		 * selectByVisibleText(year, "1992");
+		 */
+		
+		selectByVisibleText(day, "26");
+		selectByVisibleText(month, "12");
+		selectByVisibleText(year, "50");
+		
 	}
 	
 	public static void selectByVisibleText(WebElement element, String value) {
 		Select select =new Select(element);
 		select.selectByVisibleText(value);
-		
+	}
+	public static void selectByIndex(WebElement element, int index) {
+		Select select =new Select(element);
+		select.selectByIndex(index);
 	}
 
 }
